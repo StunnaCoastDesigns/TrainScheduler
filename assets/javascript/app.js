86d56d6c3ee6aef9d62782d188272fc17ab989c6
Stunna One <stunnawestcoast@icloud.com>
2019-08-20 18:04:39 -0700

    Schedule is now working with firebase, debugged all err

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
-    firebase.initializeApp(firebaseConfig);
-
+    // Your web app's Firebase configuration
     var firebaseConfig = {
         apiKey: "AIzaSyBJHLFeNSTsrIdHSQ2C1jSdmKZItHXJiQc",
         authDomain: "stunna-train-database.firebaseapp.com",
@@ -11,11 +10,13 @@ $(document).ready(function () {
         messagingSenderId: "152702923066",
         appId: "1:152702923066:web:72e50bdc8a109909"
     };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
     var database = firebase.database();
 
     // storing information from submit button
-    $(".submitInput").on("click", function (event) {
+    $(".submitInput").on("click", function () {
         console.log("function is working");
 
         // Variables
@@ -55,65 +56,65 @@ $(document).ready(function () {
         // console.log(database);
         $("input").val("");
     });
-database.ref().on("child_added", function (childSnapshot) {
-    var name = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination;
-    var time = childSnapshot.val().time;
-    var frequency = childSnapshot.val().frequency;
+    database.ref().on("child_added", function (childSnapshot) {
+        var name = childSnapshot.val().name;
+        var destination = childSnapshot.val().destination;
+        var time = childSnapshot.val().time;
+        var frequency = childSnapshot.val().frequency;
 
-    console.log(name, destination, time, frequency);
+        console.log(name, destination, time, frequency);
 
-    // time formation
+        // time formation
 
-    var frequency = parseInt(frequency);
-     var currentTime = moment();
+        var frequency = parseInt(frequency);
+        var currentTime = moment();
 
-    console.log("Current time: " + moment().format("HHmm"));
+        console.log("Current time: " + moment().format("HHmm"));
 
-    var dateConvert = moment(childSnapshot.val().time, "HHmm").subtract(1, "years");
+        var dateConvert = moment(childSnapshot.val().time, "HHmm").subtract(1, "years");
 
-    console.log("DATE CONVERTED: " + dateConvert);
-    
-    var trainTime = moment(dateConvert).format("HHmm");
+        console.log("DATE CONVERTED: " + dateConvert);
 
-    console.log("Train time : " + trainTime);
+        var trainTime = moment(dateConvert).format("HHmm");
 
-    // difference bw the times
-    var timeConvert = moment(trainTie, "HHmm").subtract(1, "years");
-    var timeDifference = moment().diff(moment(timeConvert), "minutes");
+        console.log("Train time : " + trainTime);
 
-    console.log("Difference in time: " + timeDifference);
+        // difference bw the times
+        var timeConvert = moment(trainTime, "HHmm").subtract(1, "years");
+        var timeDifference = moment().diff(moment(timeConvert), "minutes");
 
-    // remainder
-    var timeRemaining = timeDifference % frequency;
+        console.log("Difference in time: " + timeDifference);
 
-    console.log  ("Time Remaining: " + timeRemaining);
+        // remainder
+        var timeRemaining = timeDifference % frequency;
 
-    // time until next train 
-    var timeAway = frequency - timeRemaining;
+        console.log("Time Remaining: " + timeRemaining);
 
-    console.log("Minutes until next train: " + timeAway);
+        // time until next train 
+        var timeAway = frequency - timeRemaining;
 
-    // next train arrival
-    var nextArrival = moment.add(timeAway, "minutes");
+        console.log("Minutes until next train: " + timeAway);
 
-    var arrivalDisplay = moment(nextArrival).format("HHmm");
+        // next train arrival
+        var nextArrival = moment().add(timeAway, "minutes");
 
-    //append data to table
-    $("#boardText").append (
-        "<tr><td id='nameDisplay'>" + childSnapshot.val().name +
-        "<td id='destinationDisplay'>" + childSnapshot.val().destination +
-        "<td id='frequencyDisplay'>" + childSnapshot.val().frequency +
-        "<td id='arrivalDisplay'>" + childSnapshot.val().arrivalDisplay +
-        "<td id='awayDisplay'>" + timeAway + " minutes until arrival" + "</td></tr>");
-        
+        var arrivalDisplay = moment(nextArrival).format("HHmm");
+
+        //append data to table
+        $("#boardText").append(
+            "<tr><td id='nameDisplay'>" + childSnapshot.val().name +
+            "<td id='destinationDisplay'>" + childSnapshot.val().destination +
+            "<td id='frequencyDisplay'>" + childSnapshot.val().frequency +
+            "<td id='arrivalDisplay'>" + childSnapshot.val().arrivalDisplay +
+            "<td id='awayDisplay'>" + timeAway + " minutes until arrival" + "</td></tr>");
+
         console.log(arrivalDisplay);
-        console.log(tiimeAway);
+        console.log(timeAway);
 
     });
 
     //reset button
-    $(".resetInput").on("click", function(event) {
+    $(".resetInput").on("click", function (event) {
         location.reload();
     });
 
